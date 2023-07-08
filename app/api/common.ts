19551f00a91f5eaecf8dd8faa4cf1dbe9f83f298
Loveyueliang <yueliangxiaxiangyu@163.com>
2023-07-08 10:18:45 +0800

    增加模型判断切换

--- app/api/common.ts
+++ app/api/common.ts
@@ -23,8 +23,28 @@ export async function requestOpenai(req: NextRequest) {
   console.log("[Proxy] ", openaiPath);
   console.log("[Base Url]", baseUrl);
 
+  let org_id: string = "";
+
   if (process.env.OPENAI_ORG_ID) {
+    org_id = process.env.OPENAI_ORG_ID;
     console.log("[Org ID]", process.env.OPENAI_ORG_ID);
+  }
+
+  // 这里判断如果是gpt4的请求才使用组织ID，否则使用默认组织ID
+
+  // 生成一个变量来判断当前使用的模型是：
+  const clonedBody = await req.text();
+  let is_gpt_4_model: boolean = false;
+  try {
+    if (clonedBody) {
+      const jsonBody = JSON.parse(clonedBody);
+      if (!(jsonBody?.model ?? "").includes("gpt-4")) {
+        is_gpt_4_model = true;
+        org_id = "";
+      }
+    }
+  } catch (e) {
+    console.error("[OpenAI] 判断模型失败 ", e);
   }
 
   const timeoutId = setTimeout(() => {
@@ -36,40 +56,29 @@ export async function requestOpenai(req: NextRequest) {
     headers: {
       "Content-Type": "application/json",
       Authorization: authValue,
-      ...(process.env.OPENAI_ORG_ID && {
-        "OpenAI-Organization": process.env.OPENAI_ORG_ID,
+      ...(org_id.length !== 0 && {
+        "OpenAI-Organization": org_id,
       }),
     },
     cache: "no-store",
     method: req.method,
-    body: req.body,
+    body: clonedBody ? clonedBody : null,
     // @ts-ignore
     duplex: "half",
     signal: controller.signal,
   };
 
   // #1815 try to refuse gpt4 request
-  if (DISABLE_GPT4 && req.body) {
-    try {
-      const clonedBody = await req.text();
-      fetchOptions.body = clonedBody;
-
-      const jsonBody = JSON.parse(clonedBody);
-
-      if ((jsonBody?.model ?? "").includes("gpt-4")) {
-        return NextResponse.json(
-          {
-            error: true,
-            message: "you are not allowed to use gpt-4 model",
-          },
-          {
-            status: 403,
-          },
-        );
-      }
-    } catch (e) {
-      console.error("[OpenAI] gpt4 filter", e);
-    }
+  if (DISABLE_GPT4 && is_gpt_4_model) {
+    return NextResponse.json(
+      {
+        error: true,
+        message: "you are not allowed to use gpt-4 model",
+      },
+      {
+        status: 403,
+      },
+    );
   }
 
   try {
