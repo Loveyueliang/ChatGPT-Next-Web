72b68e181bb62f0ceff81e04d3d27089107e4a5f
sijinhui <sijinhui@ainnovation.com>
2023-07-15 21:25:38 +0800

    添加更新脚本

--- update.sh
+++ update.sh
@@ -7,5 +7,5 @@ docker push registry.cn-hangzhou.aliyuncs.com/sijinhui/chatgpt-next-web
 docker-compose up -d
 
 # 清理缓存
-
+sleep 2
 tccli cdn PurgePathCache --cli-unfold-argument --Paths 'https://chat.xiaosi.cc/' --FlushType delete
