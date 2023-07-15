cbca9103dc771787c55502b056fc9559a9e9615d
sijinhui <sijinhui@ainnovation.com>
2023-07-15 21:22:49 +0800

    添加更新脚本

--- update.sh
+++ update.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+source /root/miniconda3/etc/profile.d/conda.sh
+conda activate
 git pull
 docker build -t registry.cn-hangzhou.aliyuncs.com/sijinhui/chatgpt-next-web .
 docker push registry.cn-hangzhou.aliyuncs.com/sijinhui/chatgpt-next-web
 docker-compose up -d
 
 # 清理缓存
+
+tccli cdn PurgePathCache --cli-unfold-argument --Paths 'https://chat.xiaosi.cc/' --FlushType delete
